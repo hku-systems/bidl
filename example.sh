4de5c857438365f6626396876050b8dadd926779
yunpeng <ypjiang@cs.hku.hk>
2021-08-31 12:42:36 +0800

    nits

--- example.sh
+++ example.sh
@@ -8,8 +8,10 @@ docker stack deploy --compose-file=docker-compose-fastfabric.yaml fabric
 sleep 2
 # setup the channel and deploy the smart conatract
 docker exec $(docker ps | grep fabric_cli | awk '{print $1}') bash scripts/script.sh 
+sleep 2
 # create 5000 accounts
 docker exec $(docker ps | grep fabric_tape | awk '{print $1}') tape --e2e -n 50000 --burst 50000 --txtype create --num_of_conn 8 --client_per_conn 4 --send_rate 25000 --orderer_client 40 --groups 5 --config config.yaml > create.log 2>&1
+sleep 5
 # send 5000 payment transactions
 docker exec $(docker ps | grep fabric_tape | awk '{print $1}') tape --e2e -n 50000 --burst 50000 --txtype transfer --num_of_conn 8 --client_per_conn 4 --send_rate 25000 --orderer_client 40 --groups 5 --config config.yaml > transfer.log 2>&1
 # stop the network 
