c066dcf923adc926780071613c8afe5d5bdfbdcf
yunpeng <ypjiang@cs.hku.hk>
2021-09-04 15:56:47 +0800

    nits

--- setup.sh
+++ setup.sh
@@ -63,7 +63,6 @@ bash deploy.sh fastfabric-v1.0
 ## deploy streamchain
 cd streamchain/setup
 bash reconfigure.sh 
-cd $cur
 . config.sh
 for host in $peers; do
     ssh -tt $USER@$host "mkdir -p $bm_path; sudo mount -t tmpfs -o size=8G $bm_path"
